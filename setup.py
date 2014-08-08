b3a6c58032959dfb0b9fadc1b1cdb433f4b534e2
Aaron Gallagher <aaron@flowroute.com>
2014-08-08 13:55:17 -0700

    Initial commit; does nothing.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='txdockerdns',
+    description='Twisted DNS server for Docker containers',
+    author='Flowroute LLC',
+    py_modules=['txdockerdns'],
+    install_requires=[
+        'Twisted',
+    ],
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    entry_points={
+        'console_scripts': [
+            'txdockerdns = txdockerdns:main',
+        ],
+    },
+)
